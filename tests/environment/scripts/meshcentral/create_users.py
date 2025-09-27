12a3040f89a73dd3f1f24496bce146208bd807c1
Josiah Baldwin <jbaldwin8889@gmail.com>
2025-09-27 14:48:55 -0700

    Reapply "Feat/run console commands"
    This reverts commit 4cda54ab60d8a115fb9d1fb1f0461257e7a86702.

--- tests/environment/scripts/meshcentral/create_users.py
+++ tests/environment/scripts/meshcentral/create_users.py
@@ -7,9 +7,9 @@ thisdir = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(thisdir, "users.json")) as infile:
     users = json.load(infile)
 for username, password in users.items():
-    subprocess.check_output(["node", "/opt/meshcentral/meshcentral", "--createaccount", username, "--pass", password, "--name", username])
+    print(subprocess.check_output(["node", "/opt/meshcentral/meshcentral", "--createaccount", username, "--pass", password, "--name", username]))
 
 
-subprocess.check_output(["node", "/opt/meshcentral/meshcentral", "--adminaccount", "admin"])
+print(subprocess.check_output(["node", "/opt/meshcentral/meshcentral", "--adminaccount", "admin"]))
 
-subprocess.call(["bash", "/opt/meshcentral/startup.sh"])
+subprocess.call(["bash", "/opt/meshcentral/entrypoint.sh"])
